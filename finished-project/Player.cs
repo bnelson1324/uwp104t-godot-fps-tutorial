74c573d9b5e53b4a344ab293646b570edeef96a4
bnelson1324 <33214860+bnelson1324@users.noreply.github.com>
2025-03-18 05:35:46 -0700

    start work on finished project

---
+++ finished-project/Player.cs
@@ -0,0 +1,56 @@
+using Godot;
+
+namespace fpstutorial;
+
+public partial class Player : CharacterBody3D
+{
+    private const float Sensitivity = 0.002f;
+
+    [Export] private float _moveSpeed = 32;
+    [Export] private float _jumpSpeed = 12;
+
+    private Camera3D _camera;
+
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+        _camera = GetNode<Camera3D>("Camera3D");
+    }
+
+    public override void _PhysicsProcess(double deltaTime)
+    {
+        // handle walking
+        Vector2 moveInput = Input.GetVector("walk_left", "walk_right", "walk_forward", "walk_backward");
+        Vector3 moveDir = ((_camera.Basis * new Vector3(moveInput.X, 0, moveInput.Y)) with { Y = 0 }).Normalized();
+        Velocity = _moveSpeed * moveDir + Vector3.Up * Velocity.Y;
+
+        // handle jumping
+        if (Input.IsActionPressed("jump") && IsOnFloor())
+            Velocity = Velocity with { Y = 0 } + Vector3.Up * _jumpSpeed;
+
+        // handle gravity
+        Velocity += Vector3.Down * 9.8f * (float)deltaTime;
+
+        // move
+        MoveAndSlide();
+
+
+        // handle shooting
+        if (Input.IsActionPressed("shoot"))
+        {
+        }
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        // handle looking
+        if (@event is InputEventMouseMotion eventMouseMotion)
+        {
+            float xMove = eventMouseMotion.Relative.X;
+            float yMove = eventMouseMotion.Relative.Y;
+            _camera.GlobalRotation += new Vector3(-yMove, -xMove, 0) * Sensitivity;
+        }
+    }
+}
